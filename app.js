b22571af3c12920ffe70dafd95323bfdff853f85
Boris <boris.flashdev@gmail.com>
2023-05-13 14:25:12 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors  = require('cors');
+const app = express();
+
+require('dotenv').config();
+const pool = require('./db/connect');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware); 
+
+const port = process.env.PORT || 5005;
+
+pool.execute("SELECT * FROM events", function(err, result) {
+    if (err) {
+            console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    }
+});
