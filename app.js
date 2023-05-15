7669ba0fe4d978b2167d899a02684704c47b9d41
Boris <boris.flashdev@gmail.com>
2023-05-15 18:07:00 +0200

    added suth routes

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const cors  = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-require('dotenv').config();
-const pool = require('./db/connect');
+require("dotenv").config();
+const pool = require("./db/connect");
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const usersRouter = require("./routes/users");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/v1', usersRouter);
+
 app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware); 
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5005;
 
-pool.execute("SELECT * FROM events", function(err, result) {
-    if (err) {
-            console.log(err);
-    } else {
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}...`);
-        });
-    }
+pool.execute("SELECT * FROM users", function (err, result) {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  }
 });
---
+++ controllers/users.js
@@ -0,0 +1,146 @@
+const jwt = require("jsonwebtoken");
+const pool = require("../db/connect");
+const { StatusCodes } = require("http-status-codes");
+
+const signup = async (req, res) => {
+  const userName = req.headers.username;
+  const password = req.headers.password;
+  const firstName = req.headers.first;
+  const lastName = req.headers.last;
+  const emial = req.headers.emial;
+  const mobile = req.headers.mobile;
+  const job = req.headers.job;
+
+  if (!userName) {
+    console.log(userName);
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide user name." });
+    return;
+  }
+  if (!password) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide password." });
+    return;
+  }
+  if (!firstName) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide first name." });
+    return;
+  }
+  if (!lastName) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide last name." });
+    return;
+  }
+  if (!emial) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide email." });
+    return;
+  }
+  if (!mobile) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide last mobile." });
+    return;
+  }
+  if (!job) {
+    res.status(StatusCodes.BAD_REQUEST).json({ msg: "You must provide job." });
+    return;
+  }
+
+  pool.execute(
+    `
+    SELECT *
+    FROM users
+    WHERE username = "${userName}"`,
+    function (err, result) {
+      if (err || result.length > 0) {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ msg: err || `User with this Username already exists.` });
+        return;
+      } else {
+        const token = jwt.sign(
+          { userName, firstName, lastName },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "30d",
+          }
+        );
+
+        pool.execute(
+          `
+        INSERT INTO users
+        VALUES (DEFAULT, "${firstName}", "${lastName}", "${userName}", "${mobile}", "${emial}",  "${password}", NULL, NULL, "${job}", NULL, NULL, NULL, NULL, NULL, NULL, "${token}")`,
+          function (err, result) {
+            if (err || result.affectedRows === 0) {
+              res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err });
+            } else {
+              pool.execute(
+                `
+            SELECT *
+            FROM users
+            WHERE token = "${token}"`,
+                function (err, result) {
+                  if (err) {
+                    res
+                      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                      .json({ msg: err });
+                    return;
+                  } else {
+                    res
+                      .status(StatusCodes.CREATED)
+                      .json({ msg: "user created", result });
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    }
+  );
+};
+
+const login = async (req, res) => {
+  const userName = req.headers.username;
+  const password = req.headers.password;
+
+  if (!userName) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide user name." });
+    return;
+  }
+  if (!password) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "You must provide password." });
+    return;
+  }
+
+  pool.execute(
+    `
+    SELECT *
+    FROM users
+    WHERE username = "${userName}" AND password = "${password}"`,
+    function (err, result) {
+      if (err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err });
+        return;
+      } else if (result.length === 0) {
+        res.status(StatusCodes.NOT_FOUND).json({ msg: `User not Found` });
+        return;
+      } else {
+        res.status(StatusCodes.OK).json({ result });
+      }
+    }
+  );
+};
+
+module.exports = { signup, login };
